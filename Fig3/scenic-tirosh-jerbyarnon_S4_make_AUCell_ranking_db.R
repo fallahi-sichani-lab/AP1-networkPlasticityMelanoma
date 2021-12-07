83bcf9670a5865239eadecd289ab9ac272a5a127
Natacha Comandante-Lou <58699295+natacom94@users.noreply.github.com>
2021-12-07 10:57:22 -0500

    Add files via upload

---
+++ Fig3/scenic-tirosh-jerbyarnon_S4_make_AUCell_ranking_db.R
@@ -0,0 +1,34 @@
+###################################################################################################
+# (SCENIC) Make data base with AUCell rankings for Tirosh et al Jerby-Arnon data
+# Adapted from Wouters et al 2020
+# 
+# Nov, 2021
+###################################################################################################
+setwd("~/Code/Fig3/")
+library(hdf5r)
+library(AUCell)
+seed <- 123
+
+
+loomfile <- "pre-processing/jerby-arnon_sc_skin_filtered_for_scenic.loom"
+loom <- H5File$new(loomfile, mode = "r")
+
+### retrieve gene expression matrix (genes x cells)
+dgem <- t(loom[["matrix"]]$read())
+
+### get genes and cells
+genes <- loom[["row_attrs"]][["Gene"]]$read()
+rownames(dgem) <- genes
+cells <- loom[["col_attrs"]][["CellID"]]$read()
+colnames(dgem) <- cells
+
+### close loom file
+loom$close_all()
+
+# create rankings 
+set.seed(seed)
+aucellRankings <- AUCell_buildRankings(dgem, nCores = 20, plotStats = F)
+saveRDS(aucellRankings, file = "aucellRankings.rds.gz", compress = "gzip")
+
+
+sessionInfo() 
